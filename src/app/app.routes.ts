2307197bad2fcbaf18ed432480c1bd99ffbc11f3
Victoria Hurley <vhurley.dev@gmail.com>
2024-06-25 11:01:17 +0100

    layout and data initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TodosComponent } from './components/projects/todos/todos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ProfileComponent},
+    {path: 'todos', component: TodosComponent}
+];
