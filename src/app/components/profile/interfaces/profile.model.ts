78da5d3f99a906d29159c032abfbb4f19a401d98
Victoria Hurley <vhurley.dev@gmail.com>
2024-06-26 18:38:37 +0100

    portfolio service implementation

---
+++ src/app/components/profile/interfaces/profile.model.ts
@@ -0,0 +1,38 @@
+export interface Profile {
+    name: string;
+    jobTitle: string;
+    strapLine: string;
+    intros: string[];
+    experiences: Experience[];
+    technologies: Technology[];
+    projects: Project[];
+    buildInfo: BuildInfo;
+}
+
+export interface Experience {
+    dateFrom: string;
+    dateTo: string;
+    company: string;
+    title: string;
+    description: string[];
+    tech: string[];
+}
+
+export interface Technology {
+    technology: string;
+    score: string;
+}
+
+export interface Project {
+    title: string;
+    description: string;
+    img: string;
+    projectUrl: string;
+    tech: string[];
+}
+
+export interface BuildInfo {
+    intro: string;
+    repoText: string;
+    repoUrl: string;
+}
