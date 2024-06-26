11dd68b2f3986f692becbca9ebd180875d6c9cc8
Victoria Hurley <vhurley.dev@gmail.com>
2024-06-26 18:38:37 +0100

    portfolio service implementation

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { NavigationComponent } from './navigation/navigation.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { TechnologyComponent } from './technology/technology.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { Component, inject } from '@angular/core';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { TechnologyComponent } from './components/technology/technology.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProfileService } from './profile.service';
+import { CommonModule } from '@angular/common';
+import { Profile } from './interfaces/profile.model';
+import { SectionEnums } from './enums.ts/sections.enum';
+import { BuildInfoComponent } from './components/build-info/build-info.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NavigationComponent, ExperienceComponent, TechnologyComponent, ProjectsComponent],
+  imports: [CommonModule,
+            NavigationComponent,
+            ExperienceComponent, 
+            TechnologyComponent,
+            ProjectsComponent,
+            BuildInfoComponent],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  profileService = inject(ProfileService);
+  profile: Profile;
+  section: typeof SectionEnums = SectionEnums;
+
+ constructor() {
+  this.profileService.getProfile().subscribe((res: Profile) => {
+    this.profile = {...res};
+  });
+ }
 
 }
+
