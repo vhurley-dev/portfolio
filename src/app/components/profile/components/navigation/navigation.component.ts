260c5f3f39999b6e4ac4ba258860a95b03ff30d9
Victoria Hurley <vhurley.dev@gmail.com>
2024-06-27 11:47:37 +0100

    nav styling

--- src/app/components/profile/components/navigation/navigation.component.ts
+++ src/app/components/profile/components/navigation/navigation.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { SectionEnums } from '../../enums.ts/sections.enum';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { share } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
-
+  constructor(public route: ActivatedRoute){}
+  activeFragment = this.route.fragment.pipe(share()); 
+  sections: typeof SectionEnums = SectionEnums;
+  navItems: string[] = ['about', 'experience', 'projects', 'technology'];
 }
