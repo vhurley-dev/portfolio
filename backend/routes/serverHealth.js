13f7d4752f6063cb87c22fb32679ba9c16bd4d9d
Victoria Hurley <vhurley.dev@gmail.com>
2026-04-06 18:07:58 +0100

    fix: export health router

--- backend/routes/serverHealth.js
+++ backend/routes/serverHealth.js
@@ -4,3 +4,5 @@ const ServerHealthController = require("../controllers/serverHealth");
 const router = express.Router();
 
 router.get("", ServerHealthController.getServerHealth);
+
+module.exports = router;
