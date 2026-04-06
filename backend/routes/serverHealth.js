dc90852413ef3d0aea18b63040f77ef302c301e0
Victoria Hurley <vhurley.dev@gmail.com>
2026-04-06 17:51:13 +0100

    health endpoint for server heartbeat

---
+++ backend/routes/serverHealth.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const ServerHealthController = require("../controllers/serverHealth");
+
+const router = express.Router();
+
+router.get("", ServerHealthController.getServerHealth);
