2ed220e89ae0d8815a66cef5fbf815fbe9d97985
Victoria Hurley <vhurley.dev@gmail.com>
2026-04-06 18:01:10 +0100

    fe health check initial

--- client/src/app/components/projects/todos/todos.component.ts
+++ client/src/app/components/projects/todos/todos.component.ts
@@ -1,26 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { Location } from '@angular/common';
 import { TodoAddComponent } from './components/todo-add/todo-add.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { IconModule } from '../../../../../projects/icon/src/public-api';
 import { TodosService } from './todos.service';
+import { ServerStatusService } from '../../global/server-status/server-status.service';
 
 @Component({
-    selector: 'app-todos',
-    imports: [TodoAddComponent, TodoListComponent, IconModule],
-    templateUrl: './todos.component.html',
-    styleUrl: './todos.component.scss'
+  selector: 'app-todos',
+  imports: [TodoAddComponent, TodoListComponent, IconModule],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.scss',
 })
 export class TodosComponent {
+  private serverStatusService = inject(ServerStatusService);
+  isServerReady = this.serverStatusService.isReady;
   constructor(
     private _location: Location,
     private todosService: TodosService,
-  ) {}
+  ) {
+    effect(() => {
+      if (this.isServerReady()) {
+        this.todosService.getTasks(); // Call your existing data fetcher
+      }
+    });
+  }
 
   tasks = this.todosService.getUpdatedTasksListener();
 
   ngOnInit() {
-    this.todosService.getTasks();
+    // this.todosService.getTasks();
   }
 
   backClick() {
