2742f3e4dc46030b18a8e2f359b049ad8db5dcd7
Victoria Hurley <vhurley.dev@gmail.com>
2026-05-01 15:31:45 +0100

    change be port

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:3001/api',
   apiHealthUrl: 'https://portfolio-api-fbr0.onrender.com/health',
 };
