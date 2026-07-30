375b586bd3fd6d6164fe6e033f5fb04ba2991988
Victoria Hurley <vhurley.dev@gmail.com>
2026-07-30 11:17:31 +0100

    lab redesign with articles

--- client/src/app/components/projects/component-lab/interfaces/component.model.ts
+++ client/src/app/components/projects/component-lab/interfaces/component.model.ts
@@ -1,3 +1,22 @@
+export interface LabSection {
+  title: string;
+  componentName: string;
+  challenge: string;
+  solution: string;
+  developerNotes?: DeveloperNote[];
+  articles?: LabArticle[];
+}
+
+export interface DeveloperNote {
+  title: string;
+  note: string;
+}
+
+export interface LabArticle {
+  title: string;
+  url: string;
+}
+
 export interface LabTab {
   id: string;
   title: string;
